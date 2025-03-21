9f78d91f63f194a1a50f6b902d529cad9d707a4f
김연진 <kyj0719@gmail.com>
2025-03-21 17:34:32 +0900

    feat: 무한 스크롤 throttle로 수정

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,29 +3,49 @@ import Order from '../../components/common/order/order';
 import LpCard from '../../components/LpCard/LpCard';
 import { TOrder, TOrderLabel, TSearchEnum } from '../../constants/enum';
 import useGetLps from '../../hooks/queries/useGetLps';
-import { useInView } from 'react-intersection-observer';
 import ClipLoader from 'react-spinners/ClipLoader';
+import useThrottle from '../../hooks/common/useThrottle';
 
 const HomePage = () => {
     const [order, setOrder] = useState<keyof typeof TOrderLabel>(
         TOrder.NEWEST_FIRST
     );
-    const { data, isFetching, hasNextPage, fetchNextPage } = useGetLps({
-        cursor: 0,
-        order,
-        type: TSearchEnum.TITLE,
-    });
-    const { ref, inView } = useInView({
-        threshold: 0,
-    });
+    const [scrollY, setScrollY] = useState(0);
+    const throttledScrollY = useThrottle(scrollY, 300);
+
+    const { data, isFetching, hasNextPage, fetchNextPage, refetch } = useGetLps(
+        {
+            cursor: 0,
+            order,
+            type: TSearchEnum.TITLE,
+        }
+    );
 
     useEffect(() => {
-        if (inView) {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    useEffect(() => {
+        const scrollTop = window.scrollY;
+        const scrollHeight = document.documentElement.scrollHeight;
+        const clientHeight = document.documentElement.clientHeight;
+
+        if (scrollTop + clientHeight >= scrollHeight - 100) {
             if (!isFetching && hasNextPage) {
                 fetchNextPage();
             }
         }
-    }, [inView, isFetching, hasNextPage, fetchNextPage]);
+    }, [throttledScrollY, isFetching, hasNextPage, fetchNextPage]);
+
+    // order가 바뀌면 refetch
+    useEffect(() => {
+        refetch();
+    }, [order, refetch]);
 
     return (
         <div className="flex flex-col items-center border-t border-gray-800 py-5">
@@ -39,10 +59,7 @@ const HomePage = () => {
                     ))
                 )}
             </div>
-            <div
-                ref={ref}
-                className="flex w-full justify-center h-auto mt-[10px]"
-            >
+            <div className="flex w-full justify-center h-auto mt-[10px]">
                 {isFetching && <ClipLoader color={'#fff'} />}
             </div>
         </div>
